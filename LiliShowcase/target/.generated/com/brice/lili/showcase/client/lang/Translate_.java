7e9583127ad5176f646ef5d5f29a36372e3ca81d
Brice DUCARDONNOY <brice@ducardonnoy.com>
2012-11-07 21:07:04 +0100

    Initial commit

---
+++ LiliShowcase/target/.generated/com/brice/lili/showcase/client/lang/Translate_.java
@@ -0,0 +1,12 @@
+package com.brice.lili.showcase.client.lang;
+
+public class Translate_ implements com.brice.lili.showcase.client.lang.Translate {
+  
+  public java.lang.String nameField() {
+    return "Enter your name";
+  }
+  
+  public java.lang.String sendButton() {
+    return "Send";
+  }
+}
