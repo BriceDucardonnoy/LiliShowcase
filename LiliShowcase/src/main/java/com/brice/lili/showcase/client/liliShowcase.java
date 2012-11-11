b808097d5a1b18ed2dba64c8903e83e8d7e1d839
Brice DUCARDONNOY <brice@ducardonnoy.com>
2012-11-11 20:47:33 +0100

    Clean project and start begin with GWTP (doesn't compile yet)

--- LiliShowcase/src/main/java/com/brice/lili/showcase/client/liliShowcase.java
+++ LiliShowcase/src/main/java/com/brice/lili/showcase/client/liliShowcase.java
@@ -1,39 +1,41 @@
 package com.brice.lili.showcase.client;
 
 import com.allen_sauer.gwt.log.client.Log;
+import com.brice.lili.showcase.client.gin.ClientGinjector;
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.user.client.ui.IsWidget;
-import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.Widget;
-import com.sencha.gxt.core.client.resources.ThemeStyles;
-import com.sencha.gxt.core.client.resources.ThemeStyles.Styles;
-import com.sencha.gxt.widget.core.client.container.HorizontalLayoutContainer;
+import com.google.gwt.core.client.GWT;
+import com.gwtplatform.mvp.client.DelayedBindRegistry;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
-public class liliShowcase implements EntryPoint, IsWidget {
+public class liliShowcase implements EntryPoint/*, IsWidget*/ {
 
-	@UiField(provided = true)
-	Styles themeStyles = ThemeStyles.getStyle();
-
+//	@UiField(provided = true)
+//	Styles themeStyles = ThemeStyles.getStyle();
+//
 //	interface MyUiBinder extends UiBinder<Widget, MainPageUIBinder> {
 //	}
 //
 //	private static MyUiBinder uiBinder = GWT.create(MyUiBinder.class);
+	
+	private final ClientGinjector ginjector = GWT.create(ClientGinjector.class);
 
 	public void onModuleLoad() {
 		Log.setCurrentLogLevel(Log.LOG_LEVEL_INFO);
-		RootPanel.get().add(asWidget());
+		// This is required for Gwt-Platform proxy's generator
+		DelayedBindRegistry.bind(ginjector);	
+
+		ginjector.getPlaceManager().revealCurrentPlace();
+//		RootPanel.get().add(asWidget());
 	}
 
-	@Override
-	public Widget asWidget() {
-				HorizontalLayoutContainer hlc = new HorizontalLayoutContainer();
-				return hlc;
+//	@Override
+//	public Widget asWidget() {
+////		HorizontalLayoutContainer hlc = new HorizontalLayoutContainer();
+////		return hlc;
 //		return uiBinder.createAndBindUi(this);
-	}
+//	}
 
 
 
