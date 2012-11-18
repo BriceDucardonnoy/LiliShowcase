a39e5377a80efd0d58908fc249fb86fe95e0163a
Brice DUCARDONNOY <brice@ducardonnoy.com>
2012-11-18 18:37:04 +0100

    Add corps for example

--- LiliShowcase/src/main/java/com/brice/lili/showcase/client/place/NameTokens.java
+++ LiliShowcase/src/main/java/com/brice/lili/showcase/client/place/NameTokens.java
@@ -2,7 +2,7 @@ package com.brice.lili.showcase.client.place;
 
 public class NameTokens {
 
-	public static final String mainpage = "!mainpage";
+	public static final String mainpage = "mainpage";
 
 	public static String getMainpage() {
 		return mainpage;
