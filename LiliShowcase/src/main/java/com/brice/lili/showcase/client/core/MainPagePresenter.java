b8d60c7e34506d964b659feab62216a7c066132f
Brice DUCARDONNOY <brice@ducardonnoy.com>
2013-01-13 10:52:15 +0100

    Add slideshow

--- LiliShowcase/src/main/java/com/brice/lili/showcase/client/core/MainPagePresenter.java
+++ LiliShowcase/src/main/java/com/brice/lili/showcase/client/core/MainPagePresenter.java
@@ -1,19 +1,25 @@
 package com.brice.lili.showcase.client.core;
 
-import com.gwtplatform.mvp.client.Presenter;
-import com.gwtplatform.mvp.client.View;
-import com.gwtplatform.mvp.client.annotations.ProxyStandard;
-import com.gwtplatform.mvp.client.annotations.NameToken;
+import org.gwt.contentflow4gwt.client.ContentFlow;
+
 import com.brice.lili.showcase.client.place.NameTokens;
-import com.gwtplatform.mvp.client.proxy.ProxyPlace;
+import com.brice.lili.showcase.shared.model.Person;
 import com.google.inject.Inject;
 import com.google.web.bindery.event.shared.EventBus;
+import com.gwtplatform.mvp.client.Presenter;
+import com.gwtplatform.mvp.client.View;
+import com.gwtplatform.mvp.client.annotations.NameToken;
+import com.gwtplatform.mvp.client.annotations.ProxyStandard;
+import com.gwtplatform.mvp.client.proxy.ProxyPlace;
 import com.gwtplatform.mvp.client.proxy.RevealRootContentEvent;
+import com.sencha.gxt.widget.core.client.ContentPanel;
 
 public class MainPagePresenter extends
 		Presenter<MainPagePresenter.MyView, MainPagePresenter.MyProxy> {
 
 	public interface MyView extends View {
+		public ContentFlow<Person> getContentFlow();
+		public ContentPanel getMainPane();
 	}
 
 	@ProxyStandard
@@ -22,13 +28,18 @@ public class MainPagePresenter extends
 	}
 
 	@Inject
-	public MainPagePresenter(final EventBus eventBus, final MyView view,
-			final MyProxy proxy) {
+	public MainPagePresenter(final EventBus eventBus, final MyView view, final MyProxy proxy) {
 		super(eventBus, view, proxy);
 	}
 
 	@Override
 	protected void revealInParent() {
 		RevealRootContentEvent.fire(this, this);
+	}
+
+	@Override
+	protected void onReveal() {
+		super.onReveal();
+		getView().getMainPane().add(getView().getContentFlow());
 	}
 }
--- LiliShowcase/src/main/java/com/brice/lili/showcase/client/core/MainPageView.java
+++ LiliShowcase/src/main/java/com/brice/lili/showcase/client/core/MainPageView.java
@@ -1,13 +1,30 @@
 package com.brice.lili.showcase.client.core;
 
-import com.gwtplatform.mvp.client.ViewImpl;
+import org.gwt.contentflow4gwt.client.ContentFlow;
+import org.gwt.contentflow4gwt.client.ContentFlowItemClickListener;
+import org.gwt.contentflow4gwt.client.PhotoView;
+
+import com.brice.lili.showcase.shared.model.Person;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.inject.Inject;
+import com.gwtplatform.mvp.client.ViewImpl;
+import com.reveregroup.gwt.imagepreloader.client.FitImage;
+import com.sencha.gxt.widget.core.client.ContentPanel;
 
 public class MainPageView extends ViewImpl implements MainPagePresenter.MyView {
 
 	private final Widget widget;
+	private ContentFlow<Person> contentFlow;
+	
+	private Person[] PEOPLE;
+	private static final int POPUP_WIDTH = 1000;
+    private static final int POPUP_HEIGHT = 600;
+    
+//    private static final int ANIMATION_DURATION = 1000;
+    @UiField ContentPanel mainPane;
 
 	public interface Binder extends UiBinder<Widget, MainPageView> {
 	}
@@ -15,10 +32,88 @@ public class MainPageView extends ViewImpl implements MainPagePresenter.MyView {
 	@Inject
 	public MainPageView(final Binder binder) {
 		widget = binder.createAndBindUi(this);
+		
+		PEOPLE = new Person[]{
+				new Person("Steve Jobs", "photos/jobs.jpg"),
+				new Person("Bill Gates", "photos/gates.jpg"),
+				new Person("Sergey Brin", "photos/brin.jpg"),
+				new Person("Larry Page", "photos/page.jpg"),
+				new Person("John Doerr", "photos/doerr.jpg"),
+				new Person("Eric Schmidt", "photos/schmidt.jpg"),
+				new Person("Larry Wayne", "photos/wayne.jpg"),
+				new Person("Steve Wozniak", "photos/wozniak.jpg"),
+				new Person("John Cook", "photos/cook.jpg")
+		};
+//		int horizontalMargin = (Window.getClientWidth() - POPUP_WIDTH) / 2;
+//		int verticalMargin = (Window.getClientHeight() - POPUP_HEIGHT) / 2;
+//		final int initialLeft = Window.getClientWidth() + horizontalMargin;
+		contentFlow = new ContentFlow<Person>(true, true);
+		addItems(contentFlow, PEOPLE.length);
+//		final ContentFlowPopupPanel<Person> popupPanel = new ContentFlowPopupPanel<Person>(contentFlow);
+
+//        popupPanel.setSize(POPUP_WIDTH + "px", POPUP_HEIGHT + "px");
+//        popupPanel.setPopupPosition(initialLeft, verticalMargin);
+        contentFlow.addItemClickListener(new ContentFlowItemClickListener() {
+            public void onItemClicked(Widget widget) {
+                Window.alert("Clicked: ");
+            }
+        });
+//        popupPanel.show();
+//
+//        animatePopupPanel(popupPanel, initialLeft, horizontalMargin, ANIMATION_DURATION);
 	}
 
 	@Override
 	public Widget asWidget() {
 		return widget;
+	}
+	
+//	private void animatePopupPanel(final PopupPanel popupPanel, final int initialLeft, final int finalLeft, int duration) {
+//        new Animation() {
+//            @Override
+//            protected void onUpdate(double progress) {
+//                int left = (int) (((1 - progress) * initialLeft) + (progress * finalLeft));
+//                popupPanel.setPopupPosition(left, 100);
+//            }
+//        }.run(duration);
+//    }
+	
+	private void addItems(ContentFlow<Person> contentFlow, int number) {
+        for (final Person person : generatePeople(number)) {
+            contentFlow.addItems(createImageView(person));
+        }
+    }
+	
+	private PhotoView createImageView(Person person) {
+        return new PhotoView(new FitImage(person.getImageUrl()), person.getName());
+    }
+	
+	private Person[] generatePeople(int number) {
+        Person[] result = new Person[number];
+
+        for (int i = 0; i < number; i++) {
+            result[i] = PEOPLE[i % PEOPLE.length];
+        }
+
+        return result;
+    }
+	
+//	private static class ContentFlowPopupPanel<T> extends PopupPanel {
+//        private final ContentFlow<T> fContentFlow;
+//
+//        private ContentFlowPopupPanel(ContentFlow<T> contentFlow) {
+//            add(fContentFlow = contentFlow);
+//        }
+//
+//    }
+
+	@Override
+	public ContentFlow<Person> getContentFlow() {
+		return contentFlow;
+	}
+
+	@Override
+	public ContentPanel getMainPane() {
+		return mainPane;
 	}
 }
