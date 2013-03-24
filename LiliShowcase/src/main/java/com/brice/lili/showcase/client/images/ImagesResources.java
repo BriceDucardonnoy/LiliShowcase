fb837839e47c924a90db5804411c0eed944b63d1
Brice DUCARDONNOY <brice@ducardonnoy.com>
2013-03-24 20:09:30 +0100

    WIP I18N

---
+++ LiliShowcase/src/main/java/com/brice/lili/showcase/client/images/ImagesResources.java
@@ -0,0 +1,23 @@
+package com.brice.lili.showcase.client.images;
+
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.ImageResource;
+
+public interface ImagesResources extends ClientBundle {
+
+	@Source("france.png")
+	ImageResource france();
+
+	@Source("Logo.png")
+	ImageResource logo();
+	
+	@Source("Logo2.png")
+	ImageResource logo2();
+	
+	@Source("Tache.png")
+	ImageResource logoTache();
+
+	@Source("united_kingdom_great_britain.png")
+	ImageResource uk();
+
+}
