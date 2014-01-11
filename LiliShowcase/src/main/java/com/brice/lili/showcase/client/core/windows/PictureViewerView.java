9fb087a8ca2ebd8f4cffbaea0bb4e164b26a911d
Brice DUCARDONNOY <brice@ducardonnoy.com>
2014-01-11 20:43:29 +0100

    WIP viewer Button left and right styled. They still do nothing Use globalStyle style from CSS file instead of UiBinder

--- LiliShowcase/src/main/java/com/brice/lili/showcase/client/core/windows/PictureViewerView.java
+++ LiliShowcase/src/main/java/com/brice/lili/showcase/client/core/windows/PictureViewerView.java
@@ -2,6 +2,7 @@ package com.brice.lili.showcase.client.core.windows;
 
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.inject.Inject;
@@ -14,6 +15,8 @@ public class PictureViewerView extends PopupViewImpl implements PictureViewerPre
 	private final Widget widget;
 	@UiField HTMLPanel pane;
 	@UiField FitImage image;
+	@UiField Button prev;
+	@UiField Button next;
 
 	public interface Binder extends UiBinder<Widget, PictureViewerView> {
 	}
@@ -21,7 +24,11 @@ public class PictureViewerView extends PopupViewImpl implements PictureViewerPre
 	@Inject
 	public PictureViewerView(final EventBus eventBus, final Binder binder) {
 		super(eventBus);
+		
 		widget = binder.createAndBindUi(this);
+		
+		prev.setHTML("<<");// provided true if do it before createAndBindUi
+		next.setHTML(">>");
 	}
 
 	@Override
